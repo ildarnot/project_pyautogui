9e6a63e2035e91aa654914783d4ea16612c20160
Ildar <NotfullinIF@kamaz.ru>
2025-10-30 16:55:07 +0300

    Добавлено использование данных для ввода из эксель таблицы

--- first_test.py
+++ first_test.py
@@ -1,33 +1,47 @@
 import time
 import pyautogui
+import openpyxl
 # import pyscreeze
 
-pyautogui.PAUSE = 2
+pyautogui.PAUSE = 0.1
 pyautogui.FAILSAFE=True
 
 # Нажатие на кнопку запуска программы
 pyautogui.click(3709, 79)
 
+# Ожидание пока я открою полноэкранный режим
+time.sleep(2)
+
 # Нажатие на кнопку Расчёт шестерни
-pyautogui.PAUSE = 5
 # pyautogui.click(1908, 1000)
-cords=pyautogui.locateCenterOnScreen("window2.png", confidence=0.7)
-pyautogui.click(cords)
+window1=pyautogui.locateCenterOnScreen("window.png", confidence=0.7)
+pyautogui.click(window1)
 
 # Нажатие на кнопку произвести расчёт
-pyautogui.PAUSE = 0.1
 pyautogui.click(2471, 1556)
 
 # Нажатие на модуль и вписывание другого значения
-# pyautogui.PAUSE = 1
 pyautogui.click(1432, 447, 3, 0.1)
-# pyautogui.hotkey('ctrl', 'a')
-# pyautogui.press('backspace'*5)
-pyautogui.typewrite("7")
+# Открываем книгу Excel
+wb = openpyxl.load_workbook('input_data.xlsx')
+sheet = wb.active  # Или укажите название листа, если нужно
+# Читаем значение из ячейки B3
+value_from_excel = sheet['B3'].value
+pyautogui.typewrite(str(value_from_excel))
 
 pyautogui.click(1435, 503, 3, 0.1)
 pyautogui.typewrite("48")
 
+# Поиск изображения и левой верхней точки "Ширина венца"
+x, y = pyautogui.locateCenterOnScreen("window2.png", confidence=0.7)
+pyautogui.click(x+150, y, 3, 0.1)
+pyautogui.typewrite("50")
+
 # Нажатие на кнопку произвести расчёт
-# pyautogui.PAUSE = 1
 pyautogui.click(2471, 1556)
+
+# Очистка графиков
+clear=pyautogui.locateCenterOnScreen("clear_button.png", confidence=0.7)
+pyautogui.click(clear)
+
+# Построение графика с помощью 
